538e3b5b404cb789f0d0c96116f2f0bb566d7133
itarocha <itarocha@gmail.com>
2023-10-06 20:15:27 -0300

    Primeiro commit

---
+++ src/test/java/br/com/itarocha/casa/CasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itarocha.casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
